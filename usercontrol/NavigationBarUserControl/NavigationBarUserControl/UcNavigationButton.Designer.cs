43982a3de9b0f466b78da93230381926102efb1c
Gauthier Claudel <miaouzart@gmail.com>
2025-05-09 14:14:27 +0200

    Resize of the button

--- usercontrol/NavigationBarUserControl/NavigationBarUserControl/UcNavigationButton.Designer.cs
+++ usercontrol/NavigationBarUserControl/NavigationBarUserControl/UcNavigationButton.Designer.cs
@@ -36,6 +36,7 @@
             // 
             // roundedRectangle
             // 
+            this.roundedRectangle.AutoSize = true;
             this.roundedRectangle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.roundedRectangle.CornerRadius = 40F;
             this.roundedRectangle.Location = new System.Drawing.Point(-142, 8);
@@ -44,21 +45,21 @@
             this.roundedRectangle.m_EdgeSize = 20;
             this.roundedRectangle.m_FontColor = System.Drawing.Color.White;
             this.roundedRectangle.m_NBSommet = 50;
-            this.roundedRectangle.m_Size = new System.Drawing.Size(811, 279);
+            this.roundedRectangle.m_Size = new System.Drawing.Size(524, 137);
             this.roundedRectangle.m_Text = null;
             this.roundedRectangle.m_TextFont = null;
             this.roundedRectangle.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.roundedRectangle.Name = "roundedRectangle";
-            this.roundedRectangle.Size = new System.Drawing.Size(811, 279);
+            this.roundedRectangle.Size = new System.Drawing.Size(524, 137);
             this.roundedRectangle.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Crimson;
             this.pictureBox1.Image = global::NavigationBarUserControl.Properties.Resources.Surpise;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 36);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(281, 222);
+            this.pictureBox1.Size = new System.Drawing.Size(140, 102);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -67,7 +68,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Crimson;
-            this.label1.Location = new System.Drawing.Point(323, 132);
+            this.label1.Location = new System.Drawing.Point(149, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 27);
             this.label1.TabIndex = 2;
@@ -84,7 +85,7 @@
             this.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "button";
-            this.Size = new System.Drawing.Size(710, 302);
+            this.Size = new System.Drawing.Size(392, 159);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
