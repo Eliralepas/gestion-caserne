b02df8c0ccc0dfb40e67052a3b4db7d7c7361b53
Gauthier Claudel <miaouzart@gmail.com>
2025-05-10 13:35:52 +0200

    Ajout des différentte tab (resize ne marche pas à fix ?)

--- usercontrol/UC-Statistique/UC-Statistique/UCStatistique.Designer.cs
+++ usercontrol/UC-Statistique/UC-Statistique/UCStatistique.Designer.cs
@@ -28,11 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.tabStatistique = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabStatistique.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabStatistique
+            // 
+            this.tabStatistique.Appearance = System.Windows.Forms.TabAppearance.Buttons;
+            this.tabStatistique.Controls.Add(this.tabPage1);
+            this.tabStatistique.Controls.Add(this.tabPage2);
+            this.tabStatistique.Controls.Add(this.tabPage3);
+            this.tabStatistique.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabStatistique.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabStatistique.Location = new System.Drawing.Point(0, 0);
+            this.tabStatistique.Name = "tabStatistique";
+            this.tabStatistique.SelectedIndex = 0;
+            this.tabStatistique.Size = new System.Drawing.Size(800, 450);
+            this.tabStatistique.TabIndex = 0;
+            this.tabStatistique.SelectedIndexChanged += new System.EventHandler(this.tabStatistique_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 421);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Engins";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 415);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Intervention";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(792, 421);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Habilitation";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // UCStatistique
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.tabStatistique);
+            this.Name = "UCStatistique";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.tabStatistique.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabStatistique;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- usercontrol/UC-Statistique/UC-Statistique/UCStatistique.cs
+++ usercontrol/UC-Statistique/UC-Statistique/UCStatistique.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace UC_Statistique
 {
     public partial class UCStatistique: UserControl
     {
+        private SQLiteConnection _con;//connection utilisé pour récupere les statistiques
+
         public UCStatistique()
         {
             InitializeComponent();
+            tabStatistique.SelectedTab = tabPage1; // Sélectionne tabPage1
+            tabStatistique_SelectedIndexChanged(this, EventArgs.Empty); // Déclenche manuellement pour pouvoir load les données
         }
+
+        public UCStatistique(SQLiteConnection con) : this()
+        {
+            if (con.State != ConnectionState.Open)
+            {
+                throw new ArgumentException("Connection dois être ouverte");
+            }
+            _con = con;
+        }
+
+        private void tabStatistique_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (tabStatistique.SelectedTab)
+            {
+                case var tab when tab == tabPage1:
+                    MessageBox.Show("Tab 1 sélectionné");
+                    break;
+                case var tab when tab == tabPage2:
+                    MessageBox.Show("Tab 2 sélectionné");
+                    break;
+                case var tab when tab == tabPage3:
+                    MessageBox.Show("Tab 3 sélectionné");
+                    break;
+                default:
+                    MessageBox.Show("Onglet inconnu sélectionné");
+                    break;
+            }
+        }
+
+
+
     }
 }
