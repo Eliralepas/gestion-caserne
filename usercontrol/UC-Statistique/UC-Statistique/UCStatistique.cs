96e300230f5d759f7bd284894d618a405eb66b5e
Gauthier Claudel <miaouzart@gmail.com>
2025-05-10 11:04:10 +0200

    Création de l'UC statistique

---
+++ usercontrol/UC-Statistique/UC-Statistique/UCStatistique.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC_Statistique
+{
+    public partial class UCStatistique: UserControl
+    {
+        public UCStatistique()
+        {
+            InitializeComponent();
+        }
+    }
+}
