59a29ee24083fc52dc1e9439f36b2889e1279140
Diego <diego.aparicio@etu.unistra.fr>
2025-05-26 12:30:03 +0200

    Hotfix + UC-ScalableCheckBox

---
+++ usercontrol/UC-ScalableCheckBox/UC-ScalableCheckBox/UC-ScalableCheckBox.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace UC_ScalableCheckBox
+{
+    partial class ScalableCheckBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.AutoSize = true;
+            this.panel.BackColor = System.Drawing.Color.Transparent;
+            this.panel.BackgroundImage = global::UC_ScalableCheckBox.Properties.Resources.check_mark;
+            this.panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(100, 100);
+            this.panel.TabIndex = 0;
+            this.panel.Click += new System.EventHandler(this.panel_Click);
+            // 
+            // ScalableCheckBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.panel);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "ScalableCheckBox";
+            this.Size = new System.Drawing.Size(103, 103);
+            this.Resize += new System.EventHandler(this.ScalableCheckBox_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+    }
+}
